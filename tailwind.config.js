5ebc0f98e36b390ebeb32c9d893337843c86896e
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-09 16:45:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: /C:/Users/dhruv/OneDrive/Desktop/hackathon/frontend/food_loss/tailwind.config.js
+// module.exports = {
+//   content: [
+//     './src/**/*.{js,jsx,ts,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
