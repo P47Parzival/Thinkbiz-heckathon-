61fbf5fc29c7075c17dacc547e27d824c0038fa2
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-10 03:00:38 +0530

    Delete Backend directory

--- Backend/models/donation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const donationSchema = new mongoose.Schema({
-  items: [{ name: String, quantity: Number }],
-  recipient: String,
-  date: Date,
-});
-
-module.exports = mongoose.model("Donation", donationSchema);
